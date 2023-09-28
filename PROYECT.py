ca2d8c75ede9c6005a1b0deab3262d9cac315624
Santy826 <santiago.aguado@correounivalle.edu.co>
2023-09-28 10:33:24 -0500

    Segundo_Avance

--- PROYECT.py
+++ PROYECT.py
@@ -5,22 +5,31 @@ clients = 'Luis, Kevin' #Variable of type string
 def create_client(client_name): #Function for creat client
     global clients
     if client_name not in clients: # Si no esta el nombre se añade
+        _add_comma()
         clients += client_name
     else:
-        print("Client already is in the Client\'s list")
+        print("Client already is in the Client\'s list {}".format(client_name))
 
 # Devuelve la lista de los clientes actualizada
 def list_clients():
     global clients
     print(clients)
+
+def _add_comma():
+    global clients
+    clients += ", "
     
 def read_cliente(): #Function for read client
     pass
 def update_cliente(): #Function Update Client
+    
     pass
 def delete_cliente(): #Function Delete Client
+    global clients
+
     pass
-def  _get_client_name(): #Function Get Client
+def  _get_client_name(): #Function Get Client y permite preguntar por el nombre del cliente 
+
     return input("Enter your name: ").upper()
 
 
@@ -44,6 +53,7 @@ if __name__ == '__main__':
     _print_welcome()
     option = input("Type your activity: ").upper()
     if option =='C':
+        #client_name = input("Type your name client: ") Este proceso es repetitivo
         client_name = _get_client_name() #Se obtiene el nombre del cliente
         create_client(client_name) # Se envia el nombre del cliente para crear
         list_clients()
@@ -52,6 +62,7 @@ if __name__ == '__main__':
         list_clients()
     elif option == 'U':
         client_name = _get_client_name()
+        client_name_update = _get_client_name()
         print(clients)
     elif option == 'D':
         client_name = _get_client_name()
